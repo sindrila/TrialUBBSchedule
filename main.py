04732b65aa6a9c591cef82a83c83b7e930a470ef
915-Mindrila-Mihail <mihail.mindrila@stud.ubbcluj.ro>
2024-02-13 15:33:01 +0200

    testing

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-from Domain.ClassType import ClassType
 from Domain.Professor import Professor
-from Domain.ClassProfessor import ClassProfessor
-from Domain.Subject import Subject
-from Domain.ClassType import get_class_type_from_str
 from Parsers.ProfessorPageParser import ProfessorPageParser
-from Domain.Professor import get_name_from_title_and_name
-import re
 import pickle
 
-from Parsers.ProfessorScheduleParser import ProfessorScheduleParser
 
-
-def asserting(professor: Professor, professor_schedule_parser: ProfessorScheduleParser):
-    current_professor_schedule = professor_schedule_parser.get_data()
+def assert_professor_schedule(professor: Professor, current_professor_schedule: list):
     try:
         assert (f"Orar {str(professor)}" == current_professor_schedule[0])
     except AssertionError:
@@ -21,9 +12,23 @@ def asserting(professor: Professor, professor_schedule_parser: ProfessorSchedule
                 0] + " but parsed " + f"Orar {str(professor)}")
 
 
-if __name__ == "__main__":
-    test = ProfessorPageParser('https://www.cs.ubbcluj.ro/files/orar/2023-1/cadre/index.html')
-    professors = test.get_data()
-    with open ('professors_dump.pickle', 'wb') as handle:
-        pickle.dump(professors, handle, protocol=pickle.HIGHEST_PROTOCOL)
+def serialize_tuple_professor_and_classes(data) -> None:
+    with open('professors_dump.pickle', 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+
+def get_deserialized_tuple_professor_and_classes():
+    with open('professors_dump.pickle', 'rb') as handle:
+        return pickle.load(handle)
+
+
+def parse_professor_page():
+    parser = ProfessorPageParser('https://www.cs.ubbcluj.ro/files/orar/2023-1/cadre/index.html')
+    professors_and_classes_data = parser.get_data()
+    for professor_schedule in professors_and_classes_data:
+        assert_professor_schedule(professor_schedule[0], professor_schedule[1])
+    return professors_and_classes_data
+
+
+if __name__ == "__main__":
+    professors_and_classes = get_deserialized_tuple_professor_and_classes()
